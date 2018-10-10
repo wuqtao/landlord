87fc33866cc54bb6e7b9a38f27d3f0b2ae011496
wuqt <wuqingtao@lanyife.com>
2018-10-10 18:09:49 +0800

    引入扑克界面

--- views/static/js/home.js
+++ views/static/js/home.js
@@ -67,7 +67,10 @@ $(function(){
                 case TypeOfAuto:
                     break;
                 case TypeOfSendCard:
-                    console.log(data)
+
+                    $.each(data.Cards,function(i,o){
+                        $("#userCards").append(String.Format($("#tempCard").html(),o.Card.CardName,o.Card.CardSuit));
+                    })
                     break;
                 case TypeOfCallScore:
                     $("#divScore").show()
@@ -111,3 +114,29 @@ $(function(){
 
     })
 })
+
+
+String.prototype.format = function(args) {
+    var result = this;
+    if (arguments.length > 0) {
+        if (arguments.length == 1 && typeof (args) == "object") {
+            for (var key in args) {
+                if(args[key]!=undefined){
+                    var reg = new RegExp("({" + key + "})", "g");
+                    result = result.replace(reg, args[key]);
+                }
+            }
+        }
+        else {
+            for (var i = 0; i < arguments.length; i++) {
+                if (arguments[i] != undefined) {
+                    //var reg = new RegExp("({[" + i + "]})", "g");//这个在索引大于9时会有问题，谢谢何以笙箫的指出
+
+                    var reg= new RegExp("({)" + i + "(})", "g");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+        }
+    }
+    return result;
+}
